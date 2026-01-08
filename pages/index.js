4df1fc0a38191334dfb77239f780d3424260ddcd
Daniel Ferreira Veloso <45084446+Daniel-Veloso@users.noreply.github.com>
2026-01-08 20:13:20 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Este site não faz nada, e faz isso muito bem. 🙃</h1>
+function Home() {
+  return <h1>Este site não faz nada, e faz isso muito bem. 🙃</h1>;
 }
 
 export default Home;
